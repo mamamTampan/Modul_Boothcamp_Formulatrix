a9ea1d36252452b22f8cf772b0b859c79d97bfc9
mamamTampan <irfan.maoelana@gmail.com>
2023-08-03 12:08:32 +0700

    up

---
+++ Day-20/A_FileStream/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace FileHandlinDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string FilePath = @".\MyFile.txt";
+
+			using (FileStream fileStream = new FileStream(FilePath, FileMode.Create, FileAccess.Read, FileShare.Inheritable))
+			{
+				fileStream.Close();
+				Console.Write("File has been created and the Path is MyFile.txt");
+			}
+			Console.ReadKey();
+		}
+	}
+}
